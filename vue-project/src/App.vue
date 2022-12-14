ee7ed376fc33a2b892b0cc3f94224391b85ad0d3
xSami2 <109634088+xSami2@users.noreply.github.com>
2022-12-14 20:39:38 +0300

    upload

---
+++ vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import Swal from "sweetalert2";
+
+function showAlert()
+{
+  Swal.fire(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+  )
+}
+</script>
+
+<template>
+  <button @click="showAlert">Hello world</button>
+</template>
+
---
+++ vue-project/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.css";
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+const app = createApp(App)
+
+app.use(VueSweetalert2);
+
+app.mount('#app');
+import "bootstrap/dist/js/bootstrap.js";
